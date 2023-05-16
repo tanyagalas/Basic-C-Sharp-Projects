210625af36c01a7ecfc48c844c9a93d156c4c210
tanyagalas <tanyashevts@gmail.com>
2023-05-15 23:11:10 -0700

    commit

---
+++ Basic_C#_Programs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
