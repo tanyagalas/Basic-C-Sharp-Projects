9d1d924cb3bd34dced5538bda3aafe12bb5e7ec8
tanyagalas <tanyashevts@gmail.com>
2023-05-17 18:28:23 -0700

    commit

---
+++ Basic_C#_Programs/Branching/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter package weight");
+            string weight = Console.ReadLine();
+            int packageWeight = Convert.ToInt32(weight);
+
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+            }
+
+            Console.WriteLine("Please enter package width");
+            string width = Console.ReadLine();
+            int packageWidth = Convert.ToInt32(width);
+
+
+            Console.WriteLine("Please enter package height");
+            string height = Console.ReadLine();
+            int packageHeight = Convert.ToInt32(height);
+
+
+            Console.WriteLine("Please enter package length");
+            string length = Console.ReadLine();
+            int packageLength = Convert.ToInt32(length);
+            Console.ReadLine();
+
+            if (packageHeight + packageLength + packageWidth > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                System.Environment.Exit(1);
+            }
+
+            decimal sum = packageWeight * (packageHeight * packageLength * packageWidth) / 100m;
+            Console.WriteLine("Your estimated total for shipping this package is: $" + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Ternary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 10;
+
+        string output = (number > 5) ? "Number is greater than 5" : "Number is less than or equal to 5";
+
+        Console.WriteLine(output);
+    }
+}
