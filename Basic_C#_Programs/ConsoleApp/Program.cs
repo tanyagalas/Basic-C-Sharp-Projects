b77bba742687fb70a6ee4f0d1635b079d77fde06
tanyagalas <tanyashevts@gmail.com>
2023-05-18 21:41:06 -0700

    commit

---
+++ Basic_C#_Programs/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boolean comparison using a while statement
+            Console.WriteLine("Boolean comparison using a while statement:");
+            int whileCount = 0; 
+
+            while (whileCount < 5) // Execute the following block as long as whileCount is less than 5
+            {
+                Console.WriteLine("While count: " + whileCount); // Print the current value of whileCount
+                whileCount++; 
+            }
+
+            Console.WriteLine();
+
+            // Boolean comparison using a dowhile statement
+            Console.WriteLine("Boolean comparison using a do-while statement:");
+            int doWhileCount = 0;
+
+            do
+            {
+                Console.WriteLine("Do-While count: " + doWhileCount); // Print the current value of doWhileCount
+                doWhileCount++; 
+            }
+            while (doWhileCount < 5); // Execute the following block as long as doWhileCount is less than 5
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basic_C#_Programs/SWITCH CHALLENGE/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWITCH_CHALLENGE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 5:");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("You entered 1");
+                    break;
+                case 2:
+                    Console.WriteLine("You entered 2");
+                    break;
+                case 3:
+                    Console.WriteLine("You entered 3");
+                    break;
+                case 4:
+                    Console.WriteLine("You entered 4");
+                    break;
+                case 5:
+                    Console.WriteLine("You entered 5");
+                    break;
+                default:
+                    Console.WriteLine("Invalid number");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+  
