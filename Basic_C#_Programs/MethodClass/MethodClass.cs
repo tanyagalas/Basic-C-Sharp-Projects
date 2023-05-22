c9f40dce8050aa4889e67100ca4c3740f0bc174f
tanyagalas <tanyashevts@gmail.com>
2023-05-22 16:58:05 -0700

    commit

---
+++ Basic_C#_Programs/MethodClass/MethodClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassassign
+{
+    public class Method
+    {
+        // Method that takes two integers as parameters and performs a math operation
+        public void mathOp(int firstNum, int secondNum)
+        {
+            firstNum += 10;
+            Console.WriteLine("The second input is: " + secondNum);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  MethodClassassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            // Instantiate the Method class
+            Method myOp = new Method();
+
+            //Call the methOp in the class
+            myOp.mathOp(num1, num2);
+
+            //Call the methOp in the class
+            myOp.mathOp(secondNum: num2, firstNum: num1);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
