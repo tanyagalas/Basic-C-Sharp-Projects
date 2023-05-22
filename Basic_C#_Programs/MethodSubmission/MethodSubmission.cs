019fffa4a570e74bcbb4be67ef0980a4ae0570ab
tanyagalas <tanyashevts@gmail.com>
2023-05-22 15:13:16 -0700

    commit

---
+++ Basic_C#_Programs/MethodSubmission/MethodSubmission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodsubmissionassign
+{
+    class Math
+    {
+        // Method to add two integers together
+        public int AddInts(int userNum, int userNum2 = 0)
+        {
+            // Adds the two integers together
+            int newUserNum = userNum + userNum2;
+            return newUserNum;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmission/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodsubmissionassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompts the user to input an integer
+            Console.WriteLine("Please input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prompts the user to input a second integer
+            Console.WriteLine("Please input a second integer (optional): ");
+            bool userNum2IsValid = int.TryParse(Console.ReadLine(), out int userNum2);
+
+            Math myNum = new Math();
+
+            int result = 0;
+            if (userNum2IsValid)
+            {
+                result = myNum.AddInts(userNum, userNum2);
+            }
+            else
+            {
+                result = myNum.AddInts(userNum);
+            }
+
+            // Prints the result to the console
+            Console.WriteLine("Your result is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
