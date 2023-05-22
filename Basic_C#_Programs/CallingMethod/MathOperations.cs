be4570c1cc25422c4a9dc5521b4796a26d160c32
tanyagalas <tanyashevts@gmail.com>
2023-05-21 22:49:21 -0700

    commit

---
+++ Basic_C#_Programs/CallingMethod/MathOperations.cs
@@ -0,0 +1,25 @@
+﻿public class MathOperations
+{
+    // Method 1 Multiply 
+    public int MultiplyByTwo(int number)
+    {
+        return number * 2;
+    }
+
+    // Method 2 Square the parameter
+    public int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Method 3 Calculate 
+    public int CalculateFactorial(int number)
+    {
+        int factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
---
+++ Basic_C#_Programs/CallingMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user for a number
+        Console.Write("Enter a number: ");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+
+        // Create an instance of MathOperations class
+        MathOperations math = new MathOperations();
+
+        // Call each method 
+        int result1 = math.MultiplyByTwo(userInput);
+        Console.WriteLine("Result 1: " + result1);
+
+        int result2 = math.SquareNumber(userInput);
+        Console.WriteLine("Result 2: " + result2);
+
+        int result3 = math.CalculateFactorial(userInput);
+        Console.WriteLine("Result 3: " + result3);
+    }
+}
