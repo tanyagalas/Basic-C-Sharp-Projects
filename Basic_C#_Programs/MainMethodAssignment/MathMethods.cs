076bd66c48002becb118116572851e4724d81059
tanyagalas <tanyashevts@gmail.com>
2023-05-22 12:52:11 -0700

    commit

---
+++ Basic_C#_Programs/MainMethodAssignment/MathMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodassign
+{
+    class MathMethods
+    { 
+
+        //Method to add 10 to an integer and return an integer
+        public int Mathop(int number) 
+        {
+            return number + 7;
+        }
+
+        public int Mathop(decimal number)
+        {
+            int newNum = Convert.ToInt32(number);
+            return newNum * 20;
+        }
+        
+        //Method  to convert a string to an integer
+        public int Mathop(string number)
+        {
+            int newNum = Convert.ToInt32(number);
+            return newNum / 3;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            MathMethods myOp = new MathMethods();
+
+            //Example usage with integer
+            Console.WriteLine("Please input an integer to operate on: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your integer plus 7 is: " + myOp.Mathop(userNum));
+
+
+            MathMethods myOp2 = new MathMethods();//new MathOp object
+
+            //Example usage with decimal
+            Console.WriteLine("Please input a decimal to operate on: ");
+            decimal userNumFloat = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Your decimal multiplied by 20 is: " + myOp2.Mathop(userNumFloat));
+
+
+            //Usage with a string
+            MathMethods myOp3 = new MathMethods(); 
+            Console.WriteLine("Please input a integer to operate on: ");
+
+            Console.WriteLine("Your integer divided by 3 is: " + myOp3.Mathop(Console.ReadLine()));
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
