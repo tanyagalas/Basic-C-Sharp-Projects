4ff98783eb901809960cf3527daf3558c2272caa
tanyagalas <tanyashevts@gmail.com>
2023-05-22 17:49:09 -0700

    commit

---
+++ Basic_C#_Programs/ConsoleApp2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Class1
+    {
+       public int DivideByTwo(int data)
+        {
+            return data / 2;
+        }
+
+        // Method with output parameters
+        public void MethodWithOutputParams(out int param1, out int param2)
+        {
+            param1 = 10;
+            param2 = 20;
+        }
+
+        public int MethodOverload(int data)
+        {
+            return data * 2;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Class1 myClass = new Class1();
+
+            // Call the method that divides
+            int result = myClass.DivideByTwo(number);
+            Console.WriteLine("Result of division: " + result);
+
+            // Call the method with output parameters
+            int outputParam1, outputParam2;
+            myClass.MethodWithOutputParams(out outputParam1, out outputParam2);
+            Console.WriteLine("Output parameter 1: " + outputParam1);
+            Console.WriteLine("Output parameter 2: " + outputParam2);
+
+            // Call the overloaded method
+            int overloadResult = myClass.MethodOverload(number);
+            Console.WriteLine("Overloaded method result: " + overloadResult);
+
+            Console.ReadLine();
+        }
+    }
+}
