81b2929985bc43eae82547da78d40203a893d64d
tanyagalas <tanyashevts@gmail.com>
2023-05-23 20:42:27 -0700

    commit

---
+++ Basic_C#_Programs/OperatorsAssignment/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Employee : Person, IQuittable
+    {
+        public int employeeID { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Hello, my name is {0} {1}", firstName, lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("This employee has quit");
+            firstName = "";
+            lastName = "";
+        }
+
+        public static bool operator ==(Employee one, Employee two)
+        {
+            if (one.employeeID == two.employeeID)
+            {
+                Console.WriteLine("The ID's are equal");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("The ID's are not equal.");
+                return false;
+            }
+
+        }
+
+        public static bool operator !=(Employee one, Employee two)
+        {
+            if (one.employeeID == two.employeeID)
+            {
+                Console.WriteLine("The ID's are equal");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("The ID's are not equal.");
+                return true;
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorsAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Employee employee2 = new Employee();
+            employee2.firstName = "Aaron";
+            employee2.lastName = "Sampson";
+            employee2.employeeID = 3456;
+
+            Employee employee3 = new Employee();
+            employee3.firstName = "James";
+            employee3.lastName = "Davis";
+            employee3.employeeID = 7898;
+
+            bool doesEqual = employee2 == employee3; 
+
+            Console.ReadLine();
+        }
+    }
+}
