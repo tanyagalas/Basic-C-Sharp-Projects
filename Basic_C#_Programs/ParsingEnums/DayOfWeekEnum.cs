6e05d9698866cdcc16a60edc2e2ac571978f291a
tanyagalas <tanyashevts@gmail.com>
2023-05-24 14:53:50 -0700

    commit

---
+++ Basic_C#_Programs/ParsingEnums/DayOfWeekEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public enum DayOfWeekEnum
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ Basic_C#_Programs/ParsingEnums/DayOfWeekManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public static class DayOfWeekManager
+    {
+        public static void Run()
+        {
+            try
+            {
+                // Prompt the user to enter the current day of the week
+                Console.Write("Enter the current day of the week: ");
+                string userInput = Console.ReadLine();
+
+                // Convert the user input into an enum value and store it in a variable
+                DayOfWeekEnum currentDay;
+                if (Enum.TryParse(userInput, out currentDay))
+                {
+                 
+                 // User input successfully parsed
+                    Console.WriteLine("You entered: " + currentDay);
+                }
+                else
+                {
+                 // User input could not be parsed
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+            catch (Exception ex)
+            {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
