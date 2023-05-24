2970b77136319ea512c297241550418b2dd618d4
tanyagalas <tanyashevts@gmail.com>
2023-05-23 21:28:30 -0700

    commit

---
+++ Basic_C#_Programs/ParametersAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Employee<T>
+{
+    public List<T> Things { get; set; }
+
+    public Employee()
+    {
+        Things = new List<T>();
+    }
+}
---
+++ Basic_C#_Programs/ParametersAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string> { "Apple", "Banana", "Orange" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int> { 10, 20, 30, 40, 50 };
+
+            Console.WriteLine("Employee 1 Things:");
+            foreach (string item in employee1.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Employee 2 Things:");
+            foreach (int item in employee2.Things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+  
