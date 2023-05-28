30f5ec807be3459a736e1f0cfdfe2d6bf6c6ad64
tanyagalas <tanyashevts@gmail.com>
2023-05-27 19:50:56 -0700

    commit

---
+++ Basic_C#_Programs/ConsttructorAssignment/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    // A class with constructor chaining
+    class MyClass
+    {
+        public string MyProperty { get; set; }
+
+        // Constructor with a string parameter
+        public MyClass(string value)
+            : this(value, "there")
+        {
+        }
+
+        // Constructor with two string parameters
+        public MyClass(string value1, string value2)
+        {
+            MyProperty = value1 + ", " + value2 + "!";
+        }
+    }
+}
---
+++ Basic_C#_Programs/ConsttructorAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a const variable
+            const int myConstVariable = 10;
+            Console.WriteLine("Const variable value: " + myConstVariable);
+
+            // Create a variable using the keyword "var"
+            var myVarVariable = "Welcome to the program!";
+            Console.WriteLine("Var variable value: " + myVarVariable);
+
+            // Chain two constructors together
+            MyClass myObject = new MyClass("Hello");
+            Console.WriteLine("MyObject property value: " + myObject.MyProperty);
+
+            Console.ReadLine();
+        }
+    }
+}
